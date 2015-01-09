f01c864edd1448fd7d0d00de8c621f9edd13ca58
Ryan Sonnek <ryan@codecrate.com>
2015-01-08 22:29:50 -0600

    add subject to rails 4.1 mail previews

--- lib/email_prefixer/railtie.rb
+++ lib/email_prefixer/railtie.rb
@@ -1,7 +1,9 @@
 module EmailPrefixer
   class Railtie < ::Rails::Railtie
     initializer 'email_prefixer.configure_default_application_name' do |app|
-      ActionMailer::Base.register_interceptor(EmailPrefixer::Interceptor.new)
+      interceptor = EmailPrefixer::Interceptor.new
+      ActionMailer::Base.register_preview_interceptor(interceptor)
+      ActionMailer::Base.register_interceptor(interceptor)
       EmailPrefixer.configure do |config|
         config.application_name ||= app.class.parent_name
       end
